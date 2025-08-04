d1d10c1a5200f3cde3a524b0fba33e31ce2a582b
Habeba Mostafa <127840000+habebamostafa@users.noreply.github.com>
2025-08-04 18:08:48 +0300

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ def download_model():
         "1Q3WFKlNe12qXcwDnUmrrf6OkamwiXLG-": "model.safetensors", 
         "1DKsomb6RgIqombyJ3IsVemmJUu16yYDh": "special_tokens_map.json",
         "1ZM-u0_4zB21ZpL6507_ZiOm5Aa0n1x1T": "tokenizer_config.json",
-        "1X-YW8e54-O63z_oFFzZnFK54bTHBvx0y": "tokenizer_args.bin",
+        "1X-YW8e54-O63z_oFFzZnFK54bTHBvx0y": "training_args.bin",
         "1v5y-ffp9O6FW7T3G2tST26O1RmdugxXf": "vocab.txt"
     }
 
@@ -41,13 +41,13 @@ def download_model():
 @st.cache_resource
 def load_model():
     # Ensure model_path is a valid Hugging Face model ID or a local path
-    model_path = download_model()
+    download_model()
     model = BertForSequenceClassification.from_pretrained(
-        model_path,
+        ".",
         local_files_only=True ,# Explicitly specify loading from local files
     )
     tokenizer = BertTokenizer.from_pretrained(
-        model_path,
+        ".",
         local_files_only=True # Explicitly specify loading from local files
     )
 
